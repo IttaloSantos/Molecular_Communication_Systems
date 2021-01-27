b45f22b5d4f11baf5ae67208dd362b2cbfebf2f4
IttaloSantos <ittalosantoss@gmail.com>
2021-01-26 23:16:56 -0300

    Creating images and adding annotation text in graph

--- graphs.py
+++ graphs.py
@@ -7,9 +7,9 @@ timeSlotStep       = 0.01
 timeSlotBegin      = 0.01
 timeSlotEnd        = 0.11
 
-freq               = np.around(np.arange(0.1, 1.1, 0.1), 1             )
-timeSlot           = np.arange(timeSlotBegin, timeSlotEnd, timeSlotStep)
-rxRange            = np.arange(1,7                                     )
+freq               = np.around(np.arange(0.1, 1.1, 0.1), 1                           )
+timeSlot           = np.around(np.arange(timeSlotBegin, timeSlotEnd, timeSlotStep), 2)
+rxRange            = np.arange(1,7                                                   )
 
 ### Setting the analysis ###
 # evalMetricName     = 'Channel Capacity (bits)'
@@ -17,27 +17,28 @@ rxRange            = np.arange(1,7                                     )
 # evalMetricName     = 'SNR (dB)'
 evalMetricName     = 'BER'
 
-referenceVariable  = 'Time Slot (s)'
-# yAxisVariable      = 'Range'
+# referenceVariable  = 'Time Slot (s)'
+referenceVariable      = 'Range'
 
-referenceNumber    = 0.06
+referenceNumber    = 1
 
-xAxisVariable      = rxRange                                                # The opposite of referenceVariable
-# xAxisVariable      = timeSlot
+# xAxisVariable      = rxRange                                                # The opposite of referenceVariable
+xAxisVariable      = freq
 
-xAxisName          = 'r (#celulas)'                                         # The same as xAxisVariable
-# xAxisName          = 'Tb (s)'
+# xAxisName          = 'r (#celulas)'                                         # The same as xAxisVariable
+xAxisName          = 'f (Hz)'
+
                     #   Red        Orange     Yellow     Green      Brown      Blue      Blue Sky    Pink     Green Olive   Black               
 lineColor          = ['#FF0000', '#FFA500', '#FFFF00', '#008000', '#8B4513', '#0000FF', '#00BFFF', '#FF1493', '#808000', '#000000']  # 10 colors for Time Slot and Frequency
 
 ### Select the file ###
-fileName           = "results_NCX_CP.csv"
+# fileName           = "results_NCX_CP.csv"
 # fileName           = "results_NCX_CP_04.csv"
 # fileName           = "results_NCX_CP_05.csv"
-# fileName           = "results_NCX_NKP.csv"
+fileName           = "results_NCX_NKP.csv"
 
 ### Creating DataFrame from CSV file ###
-results            = pd.read_csv("results/"+fileName                   )
+results            = pd.read_csv("results/" + fileName                               )
 
 ### Processing data ###
 resultsMean        = results.groupby(['Freq (Hz)', 'Range', 'Time Slot (s)']).mean()
@@ -45,9 +46,13 @@ resultsMean        = results.groupby(['Freq (Hz)', 'Range', 'Time Slot (s)']).me
 ### Graphs of the evaluation Metric ###
 fig = go.Figure()
 
-for f, c in zip(freq, lineColor):
+# for f, c in zip(freq, lineColor):
     
-    evalMetric = resultsMean.groupby(['Freq (Hz)', referenceVariable]).get_group( (f, referenceNumber) )
+#     evalMetric = resultsMean.groupby(['Freq (Hz)', referenceVariable]).get_group( (f, referenceNumber) )
+
+for ts, c in zip(timeSlot, lineColor):
+    
+    evalMetric = resultsMean.groupby(['Time Slot (s)', referenceVariable]).get_group( (ts, referenceNumber) )
 
     # fig.add_trace(go.Scatter(
     #     x=x+x_rev,
@@ -63,17 +68,25 @@ for f, c in zip(freq, lineColor):
         x          = xAxisVariable, 
         y          = np.asarray( evalMetric[ evalMetricName ] ),
         line_color = c,
-        name       = str(f) + " Hz",
+        name       = str(ts) + " s",                                                              # If frequency is the main variable
     ))
 
-fig.update_traces(mode='lines')
+fig.update_traces(mode='lines+markers', marker_size=5)
 
-fig.update_layout(xaxis_title = '<b>' + xAxisName + '<b>',
-                yaxis_title = '<b>' + evalMetricName + '<b>',
-                yaxis_zeroline = False, 
+fig.update_layout(xaxis_title  = '<b>' + xAxisName + '<b>',
+                yaxis_title    = '<b>' + evalMetricName + '<b>',
+                yaxis_zeroline = False,
                 xaxis_zeroline = False,
-                font=dict(size = 16),
-                paper_bgcolor = 'rgba(0,0,0,0)',plot_bgcolor = 'rgba(0,0,0,0)')
+                font           = dict(size = 16),
+                paper_bgcolor  = 'rgba(0,0,0,0)',plot_bgcolor = 'rgba(0,0,0,0)')
+
+xAxisVariableMaxValue = max( xAxisVariable )
+evalMetricMaxValue    = max( np.asarray( evalMetric[ evalMetricName ] ) )
+
+fig.add_annotation(x  = xAxisVariableMaxValue - xAxisVariableMaxValue / 1.5,                       
+            y         = evalMetricMaxValue -  evalMetricMaxValue / 8,
+            text      = '<b>r = ' + str(referenceNumber) + ' célula(s)<b>',                              # Depending on the reference variable
+            showarrow = False)
 
 fig.update_yaxes(showline=True, linewidth=2, linecolor='black', showgrid=True, gridwidth=1, gridcolor='lightgray')
 fig.update_xaxes(showline=True, linewidth=2, linecolor='black', showgrid=True, gridwidth=1, gridcolor='lightgray')
