a55fcafdf5469300f28787781171ec1ecb195844
IttaloSantos <ittalosantoss@gmail.com>
2021-07-04 22:44:24 -0300

    Implemented interpolation for smoothing curves

--- graphs.py
+++ graphs.py
@@ -1,12 +1,15 @@
+from numpy.core.fromnumeric import shape
 import pandas            as pd
 import numpy             as np
 import plotly.graph_objs as go
 import matplotlib.pyplot as plt
+from scipy import interpolate
 
 ### Setting Simulation Variables ###
 timeSlotStep       = 1e-4
 timeSlotBegin      = 5e-4
 timeSlotEnd        = 1.1e-3
+interSamplesNumber = 12
 
 freq               = np.around(np.arange(0.1, 1.1, 0.1), 1                           )
 timeSlot           = [5e-4,6e-4,7e-4,8e-4,9e-4,1e-3,2e-3,3e-3,4e-3,5e-3] #np.around(np.arange(timeSlotBegin, timeSlotEnd, timeSlotStep), 4)
@@ -14,31 +17,31 @@ rxRange            = np.arange(1,7                                              
 
 ### Setting the analysis ###
 # evalMetricName     = 'Channel Capacity (bits)'
-evalMetricName     = 'Gain (dB)'
+# evalMetricName     = 'Gain (dB)'
 # evalMetricName     = 'SNR (dB)'
-# evalMetricName     = 'BER'
+evalMetricName     = 'BER'
 
-referenceVariable  = 'Time Slot (s)'
-# referenceVariable  = 'Range'                                              # Variable that will be showed in the text
+# referenceVariable  = 'Time Slot (s)'
+referenceVariable  = 'Range'                                              # Variable that will be showed in the text
 
-referenceNumber    = 2e-3                                                    # The value for the referenceVariable that will be considered on the results
+referenceNumber    = 1                                                    # The value for the referenceVariable that will be considered on the results
 
-xAxisVariable      = rxRange                                              # The opposite of referenceVariable
-# xAxisVariable      = freq
+# xAxisVariable      = rxRange                                              # The opposite of referenceVariable
+xAxisVariable      = freq
 # xAxisVariable      = timeSlot
 
-xAxisName          = 'r (#celulas)'                                       # The same as xAxisVariable
-# xAxisName          = 'f (Hz)'
+# xAxisName          = 'r (#celulas)'                                       # The same as xAxisVariable
+xAxisName          = 'f (Hz)'
 # xAxisName          = 'Tb (s)'
 
                     #   Red        Orange     Yellow     Green      Brown      Blue      Blue Sky    Pink     Green Olive   Black               
 lineColor          = ['#FF0000', '#FFA500', '#FFFF00', '#008000', '#8B4513', '#0000FF', '#00BFFF', '#FF1493', '#808000', '#000000']  # 10 colors for Time Slot and Frequency
 
 ### Select the file ###
-fileName           = "C.csv"
-# fileName           = "NCX_Na.csv"
-# fileName           = "NCX_Na_NKP_CP.csv"
-# fileName           = "NCX_NKP_CP.csv"
+# fileName           = "C.csv"
+# fileName           = "Na_NCX.csv"
+# fileName           = "Na_NCX_NKP_CP.csv"
+fileName           = "NCX_NKP_CP.csv"
 
 ### Creating DataFrame from CSV file ###
 results            = pd.read_csv("results/" + fileName                               )
@@ -51,12 +54,10 @@ fig = go.Figure()
 figs, ax = plt.subplots()
 
 for f, c in zip(freq, lineColor):
-    
     evalMetric = resultsMean.groupby(['Freq (Hz)', referenceVariable]).get_group( (f, referenceNumber) )
 
-    # for ts, c in zip(timeSlot, lineColor):
-        
-    #     evalMetric = resultsMean.groupby(['Time Slot (s)', referenceVariable]).get_group( (ts, referenceNumber) )
+# for ts, c in zip(timeSlot, lineColor):    
+#     evalMetric = resultsMean.groupby(['Time Slot (s)', referenceVariable]).get_group( (ts, referenceNumber) )
 
     # fig.add_trace(go.Scatter(
     #     x=x+x_rev,
@@ -68,41 +69,46 @@ for f, c in zip(freq, lineColor):
     #     name='tx: [Ca2+]i',
     # ))
 
-    fig.add_trace(go.Scatter(
-        x          = xAxisVariable, 
-        y          = np.asarray( evalMetric[ evalMetricName ] ),
-        line_color = c,
-        name       = str(f) + " Hz",                                                              # If frequency is the main variable
-    ))
+    y_1 = np.array(evalMetric[ evalMetricName ])
 
-    # ax.semilogy(np.asarray( evalMetric['SNR (dB)'] ), np.asarray( evalMetric[evalMetricName] ) ) # , label=legendas[i], linestyle=estilos_linha[i]
-#     ax.semilogx(timeSlot, np.asarray( evalMetric[evalMetricName] ) )
+    # cubic_interp = interpolate.interp1d(xAxisVariable, y_1)
+    # xnew         = np.linspace(min(xAxisVariable), max(xAxisVariable), interSamplesNumber)
 
-# # ax.set(xlabel='SNR (dB)', ylabel='BER')
-# ax.set(xlabel='Tb (s)', ylabel='BER')
-# ax.grid()
-# ax.legend(loc=1)
-# plt.savefig("ber_c1_TbxF.png")
-# plt.show()
+    # fig.add_trace(go.Scatter(
+    #     x          = xAxisVariable, 
+    #     y          = cubic_interp(xnew),# y_1, 
+    #     line_color = c,
+    #     name       = str(f) + " Hz",                                                              # If frequency is the main variable
+    # ))
 
-fig.update_traces(mode='lines+markers', marker_size=5)
+    # ax.semilogy(np.asarray( evalMetric['SNR (dB)'] ), y_1 ) # , label=legendas[i], linestyle=estilos_linha[i]
+    ax.semilogx(timeSlot, y_1, color = c, label = str(f) + " Hz")
 
-fig.update_layout(xaxis_title  = '<b>' + xAxisName + '<b>',
-                yaxis_title    = '<b>' + evalMetricName + '<b>',
-                yaxis_zeroline = False,
-                xaxis_zeroline = False,
-                font           = dict(size = 16),
-                paper_bgcolor  = 'rgba(0,0,0,0)',plot_bgcolor = 'rgba(0,0,0,0)')
+# ax.set(xlabel='SNR (dB)', ylabel='BER')
+ax.set(xlabel='Tb (s)', ylabel='BER')
+ax.grid()
+ax.legend()
+plt.savefig("ber_r1_TbxF.png")
+plt.show()
 
-xAxisVariableMaxValue = max( xAxisVariable )
-evalMetricMaxValue    = max( np.asarray( evalMetric[ evalMetricName ] ) )
+# fig.update_traces(mode='lines+markers', marker_size=5)
 
-fig.add_annotation(x  = xAxisVariableMaxValue - 0.5*xAxisVariableMaxValue,# / 1.5,                       
-            y         = evalMetricMaxValue,# -  evalMetricMaxValue / 8,
-            text      = '<b>Tb = ' + str(referenceNumber) + ' s<b>',                              # Depending on the reference variable
-            showarrow = False)
+# fig.update_layout(xaxis_title  = '<b>' + xAxisName + '<b>',
+#                 yaxis_title    = '<b>' + evalMetricName + '<b>', # 'Ganho (dB)' 'Capacidade do canal (bps)',  
+#                 yaxis_zeroline = False,
+#                 xaxis_zeroline = False,
+#                 font           = dict(size = 16),
+#                 paper_bgcolor  = 'rgba(0,0,0,0)',plot_bgcolor = 'rgba(0,0,0,0)')
 
-fig.update_yaxes(showline=True, linewidth=2, linecolor='black', showgrid=True, gridwidth=1, gridcolor='lightgray')
-fig.update_xaxes(showline=True, linewidth=2, linecolor='black', showgrid=True, gridwidth=1, gridcolor='lightgray')
+# xAxisVariableMaxValue = max( xAxisVariable )
+# evalMetricMaxValue    = max( np.asarray( evalMetric[ evalMetricName ] ) )
 
-fig.show()
+# # fig.add_annotation(x  = xAxisVariableMaxValue - 0.5*xAxisVariableMaxValue,# / 1.5,                       
+# #             y         = evalMetricMaxValue,# -  evalMetricMaxValue / 8,
+# #             text      = '<b>r = ' + str(referenceNumber) + ' Célula<b>',                              # Depending on the reference variable
+# #             showarrow = False)
+
+# fig.update_yaxes(showline=True, linewidth=2, linecolor='black', showgrid=True, gridwidth=1, gridcolor='lightgray')
+# fig.update_xaxes(showline=True, linewidth=2, linecolor='black', showgrid=True, gridwidth=1, gridcolor='lightgray')
+
+# fig.show()
